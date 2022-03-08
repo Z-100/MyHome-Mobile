cd494d8e62848495a8de83f7670ec76cb2537ed0
marvin <marvin131t@gmail.com>
2022-03-08 16:21:59 +0100

    Wireframe: xml + kotlin

---
+++ app/src/main/java/com/myhome/fragment/HouseFragment.kt
@@ -0,0 +1,33 @@
+package com.myhome.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.myhome.databinding.FragmentHouseBinding
+import com.myhome.databinding.FragmentLoginBinding
+
+class HouseFragment : Fragment() {
+
+    private var _binding: FragmentHouseBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        _binding = FragmentHouseBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
