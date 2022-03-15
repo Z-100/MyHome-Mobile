ff1a797d474f94218f19a8faf9b166c964fe97d0
marvin <marvin131t@gmail.com>
2022-03-15 16:33:45 +0100

    Added functionality to read data from SharedPreferences

--- app/src/main/java/com/myhome/blueprint/Account.java
+++ app/src/main/java/com/myhome/blueprint/Account.java
@@ -1,28 +1,47 @@
 package com.myhome.blueprint;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Please lombok... please just work with kotlin
+ * I really really miss you
  */
+@Getter
+@Setter
 public class Account {
 
     private String email;
     private String password;
+    private String token;
 
-    public Account(String email, String password) {
+    public Account(String email, String password, String token) {
         this.email = email;
         this.password = password;
+        this.token = token;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
     }
 
     public String getEmail() {
         return email;
     }
-    public void setEmail(String email) {
-        this.email = email;
-    }
+
     public String getPassword() {
         return password;
     }
-    public void setPassword(String password) {
-        this.password = password;
+
+    public String getToken() {
+        return token;
     }
 }
--- app/src/main/java/com/myhome/other/Constants.kt
+++ app/src/main/java/com/myhome/other/Constants.kt
@@ -3,24 +3,57 @@ package com.myhome.other
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 
+// API Constants
+class ApiConstants {
+    companion object Factory {
+        // ! Replace by server IP
+        const val BASE_URL = "http://10.62.110.24:8080"
+
+        // Actual constants
+        const val REGISTRATION_TOKEN = "MAHANSH MUTEM blyat suk my dik"
+        const val LOGIN_TOKEN = "ma-ta este super dracu, blyat"
+
+        const val DEFAULT_HOME_NAME = "Home"
+        const val NEW_HOUSE_FIELD = "newHouseName"
+        const val DEFAULT_MEMBER_FIELD = "defaultMemberName"
+        const val INVALIDCREDENTIALSERROR = "Invalid Credentials"
+
+        const val EMAIL_FIELD = "email"
+        const val PASSWORD_FIELD = "password"
+        const val TOKEN_FIELD = "token"
+        const val MEMBER_ID_FIELD = "member-id"
+        const val ICON_FIELD = "icon"
+        const val NAME_FIELD = "name"
+        const val REPLACEMENT_ICON_FIELD = "replacement-icon"
+        const val REPLACEMENT_NAME_FIELD = "replacement-name"
+        const val ID_FIELD = "id"
+        const val MEMBER_FIELD = "Members"
+        const val GET_RATING_FIELD = "get-rating"
+
+        const val TAG = "api"
+        const val DEFAULT_ICON = "3"
+    }
+}
+
+class ApiError {
+    companion object Factory {
+        const val GENERAL_ERROR = "Something went wrong. Please try again later"
+    }
+}
+
+// Fragment Constants
 class Constants {
     companion object Factory {
-        const val killAndy: String = "Kill him"
+
     }
 }
 
 class SharedPreferencesStrings {
     companion object Factory {
-        const val EMAIL = "email"
-        const val PASSWORD = "password"
-    }
-}
+        const val SHARED_PREF_NAME = "user-preferences"
 
-class Snackbar {
-    companion object Factory {
-        fun displaySnackbar(s: String, view: View) {
-            Snackbar.make(view, s, Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        const val EMAIL = "e-mail"
+        const val PASSWORD = "password"
+        const val TOKEN = "token"
     }
 }
