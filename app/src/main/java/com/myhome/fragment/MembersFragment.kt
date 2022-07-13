b283f04260589c09cb2b6e6b25616bb78f119b3f
Marvin <marvin131t@gmail.com>
2022-07-13 17:26:37 +0200

    Updated fragments

--- app/src/main/java/com/myhome/fragment/MembersFragment.kt
+++ app/src/main/java/com/myhome/fragment/MembersFragment.kt
@@ -1,23 +1,43 @@
 package com.myhome.fragment
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.myhome.R
 import com.myhome.databinding.FragmentMembersBinding
 
+import org.json.JSONException
+
+import org.json.JSONArray
+
+import com.myhome.service.api.components.impl.AccountApiService
+import com.myhome.blueprint.Member
+import com.myhome.other.GridAdapter
+import com.myhome.other.SpStrings
+import com.myhome.service.data.DataHandlingService
+import java.lang.Exception
+
+import android.widget.AdapterView.OnItemClickListener
+import com.myhome.other.Session
+
 /**
- * @author z-100
+ * @author Z-100
  */
 class MembersFragment : Fragment() {
 
     private var _binding: FragmentMembersBinding? = null
-
     private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    private var accountService = AccountApiService()
+    private var dataService = DataHandlingService()
+
+    private val members = ArrayList<Member>()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
 
         _binding = FragmentMembersBinding.inflate(inflater, container, false)
         return binding.root
@@ -26,6 +46,61 @@ class MembersFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        getMembersFromApi()
+        // Debug only. Remove
+            members.add(Member(1, "San", 2))
+            members.add(Member(2, "ss", 2))
+            members.add(Member(3, "sas", 1))
+            members.add(Member(4, "asd", 3))
+        generateBindings()
+    }
+
+    private fun generateBindings() {
+        binding.addMemberButton.setOnClickListener {
+            findNavController().navigate(R.id.members_to_add_member)
+        }
+
+        val gridAdapter = GridAdapter(context, members, R.layout.member_grid_item)
+        binding.gridView.adapter = gridAdapter
+
+        binding.gridView.onItemClickListener = OnItemClickListener {
+                _, _, position, _ ->
+
+            Session.create(members[position], members)
+
+            findNavController().navigate(MembersFragmentDirections
+                .membersToDashboard().setBackButton(R.id.dashboard_to_members))
+        }
+    }
+
+    private fun getMembersFromApi() {
+        members.clear()
+
+        val sp = context!!.getSharedPreferences(SpStrings.GENERAL, Context.MODE_PRIVATE)
+        val account = dataService.loadData(sp)
+
+        try {
+            val email = account!!.email
+            val token = account.token
+
+            accountService.getAllMembers(context, email, token) { result ->
+                parseMembers(result)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    @Throws(JSONException::class)
+    fun parseMembers(members: JSONArray) {
+        for (i in 0 until members.length()) {
+            val member = members.getJSONObject(i)
+            this.members.add(Member(
+                member.getLong("id"),
+                member.getString("name"),
+                member.getInt("icon")
+            ))
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/myhome/fragment/RegisterFragment.kt
+++ app/src/main/java/com/myhome/fragment/RegisterFragment.kt
@@ -1,20 +1,31 @@
 package com.myhome.fragment
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import com.myhome.R
+import com.myhome.service.api.components.impl.AccountApiService
+import com.myhome.blueprint.Account
 import com.myhome.databinding.FragmentRegisterBinding
+import com.myhome.other.ApiConstants
+import com.myhome.other.SpStrings
+import com.myhome.other.Strings
+import com.myhome.service.data.DataHandlingService
+import java.lang.Exception
 
-/**
- * @author z-100
- */
 class RegisterFragment : Fragment() {
 
     private var _binding: FragmentRegisterBinding? = null
-
     private val binding get() = _binding!!
+
+    private var accountService = AccountApiService()
+    private var dataService = DataHandlingService()
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -26,6 +37,64 @@ class RegisterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        generateBindings()
+    }
+
+    private fun generateBindings() {
+        binding.submitButton.isEnabled = false
+        binding.submitButton.setOnClickListener {
+            if (noFieldEmpty()) register()
+             else Snackbar.make(view!!, Strings.FILL_IN_ALL_FIELDS, Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+
+        }
+
+        binding.loginInsteadButton.setOnClickListener {
+            findNavController().navigate(R.id.register_to_login)
+        }
+
+        binding.inputEmail.doOnTextChanged {
+                _,_,_,_ -> binding.submitButton.isEnabled = noFieldEmpty()
+        }
+
+        binding.inputPassword.doOnTextChanged {
+                _,_,_,_ -> binding.submitButton.isEnabled = noFieldEmpty()
+        }
+
+        binding.inputRepeatPassword.doOnTextChanged {
+                _,_,_,_ -> binding.submitButton.isEnabled = noFieldEmpty()
+        }
+    }
+
+    private fun register() {
+        val email: String = binding.inputEmail.text.toString()
+        val password: String = binding.inputPassword.text.toString()
+        val rPassword: String = binding.inputRepeatPassword.text.toString()
+
+        if (password != rPassword)
+            return
+
+        try {
+            accountService.registerNewAccount(context, email, password, Strings.DEFAULT_MEMBER_NAME) {
+                    result ->
+                val sp = context!!.getSharedPreferences(SpStrings.GENERAL, Context.MODE_PRIVATE)
+                val account = Account(email, password, result.getString(ApiConstants.TOKEN_FIELD))
+
+                dataService.saveData(sp, account)
+
+                findNavController().navigate(R.id.register_to_members)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Snackbar.make(view!!, Strings.USERNAME_ALREADY_TAKEN, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    private fun noFieldEmpty(): Boolean {
+        return binding.inputEmail.text.toString().trim().isNotEmpty()
+                && binding.inputPassword.text.toString().trim().isNotEmpty()
+                && binding.inputRepeatPassword.text.toString().trim().isNotEmpty()
     }
 
     override fun onDestroyView() {
